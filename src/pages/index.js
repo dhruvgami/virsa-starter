2b7d5bb89815e01e188c7bc7f163617cbea65e2d
Dhruv Gami <dhruvgami@gmail.com>
2019-08-20 02:08:09 +0000

    Removing extra content

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,17 +30,7 @@ class IndexPage extends React.Component {
             justifyContent: 'space-between',
           }}
         >
-        <div class="content-wrapper">
-            <div class="content-heading">
-               <div>Celebrating Global Heritage<small>Passing down traditions!</small></div>
-               <div class="ml-auto">
-                  <div class="btn-group"><button class="btn btn-secondary dropdown-toggle dropdown-toggle-nocaret" type="button" data-toggle="dropdown">English</button>
-                     <div class="dropdown-menu dropdown-menu-right-forced animated fadeInUpShort" role="menu"><a class="dropdown-item" href="#" data-set-lang="en">English</a><a class="dropdown-item" href="#" data-set-lang="es">Spanish</a></div>
-                  </div>
-               </div>
-            </div>
-        </div>
-
+        
         <div class="content-wrapper">
             <div class="content-heading">
                <div>Celebrating Global Heritage<small>Passing down traditions!</small></div>
@@ -248,76 +238,8 @@ class IndexPage extends React.Component {
  */}
             </div>
     </div>
-          <div>
-            <p>
-              Welcome to your new Gatsby + Netlify Functions + Netlify Identity
-              site
-            </p>
-            <ul>
-              <li>
-                This site has statically generated marketing pages like this one
-                and <Link to="/page-2/">page 2.</Link>{' '}
-              </li>
-              <li>
-                It also has a dynamically generated clientside app guarded by
-                authentication:
-                <ul>
-                  <li>
-                    <Link to="/app/">
-                      <b>Go to App (with Netlify Identity)</b>
-                    </Link>{' '}
-                  </li>
-                </ul>
-              </li>
-              <li>
-                You can{' '}
-                <a href="https://github.com/sw-yx/jamstack-hackathon-starter">
-                  view source here
-                </a>
-              </li>
-              <li>
-                or see{' '}
-                <a href="https://youtu.be/bueXJInQt2c">
-                  the Youtube walkthrough
-                </a>
-              </li>
-              <li>
-                or
-                <a href="https://app.netlify.com/start/deploy?repository=https://github.com/sw-yx/jamstack-hackathon-starter&stack=cms">
-                  <img
-                    src="https://www.netlify.com/img/deploy/button.svg"
-                    alt="Deploy to Netlify"
-                  />
-                </a>
-              </li>
-            </ul>
-            <hr />
-            <p>
-              You can still access Netlify functions even on static "marketing
-              pages":{' '}
-            </p>
-            <button onClick={this.handleClick}>
-              {loading ? 'Loading...' : 'Call Lambda Function'}
-            </button>
-            <br />
-            <pre>
-              {msg
-                ? 'Here is the response: ' + msg
-                : 'click the button and watch this!'}
-            </pre>
-          </div>
-          <div
-            style={{
-              borderLeft: 'brown',
-              borderLeftStyle: 'dashed',
-              paddingLeft: '3rem',
-            }}
-          >
-            <p>Now go build something great.</p>
-            <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-              <Image />
-            </div>
-          </div>
+
+
         </div>
       </Layout>
     )
