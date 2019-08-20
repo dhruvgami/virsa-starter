b1f8280efc75e694d4325675eb04279cf23d7d67
Dhruv Gami <dhruvgami@gmail.com>
2019-08-20 01:51:26 +0000

    Testing more content

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,7 @@ class IndexPage extends React.Component {
                </div>
             </div>
         </div>
-{/*
+
         <div class="content-wrapper">
             <div class="content-heading">
                <div>Celebrating Global Heritage<small>Passing down traditions!</small></div>
@@ -51,102 +51,101 @@ class IndexPage extends React.Component {
                </div>
             </div>
 
-         
-         
-               <div class="col-xl-12">
-                     <h3>Recently Added</h3>
+           <div class="col-xl-12">
+                 <h3>Recently Added</h3>
 
-                     <div class="row">
-          
-                     <div class="col-xl-4 col-lg-6">
-                           <div class="card b">
-                              <div class="card-header">
-                                 <div class="float-right">
-                                    <div class="badge badge-info">Popular</div>
-                                    <div class="badge badge-danger">New</div>
-                                 </div>
-                                 <h4 class="m-0">Santana - Oye Como Va</h4><small class="text-muted">&nbsp;</small>
-                              </div>
-                              <div class="card-body">
-                                 <div class="d-flex align-items-center">
-                                    <img class="card-image" src="img/music/cincodemayo.png" alt="cinco de mayo" />
-                                 </div>
-                              </div>
-                              <table class="table">
-                                 <tbody>
-                                    <tr>
-                                       <td><strong>Source Link</strong></td>
-                                       <td><a href="">Santana - Oye Como Va</a></td>
-                                    </tr>
-                                    <tr>
-                                       <td><strong>Related content</strong></td>
-                                       <td>
-                                          <a href="" data-toggle="tooltip" data-title="Dances" data-original-title="" title=""><em class="fas fa-hands">&nbsp;</em></a>
-                                          <a href="" data-toggle="tooltip" data-title="Dresses" data-original-title="" title=""><em class="fas fa-female">&nbsp;</em></a>
-                                          <a href="" data-toggle="tooltip" data-title="Festivals" data-original-title="" title=""><em class="fas fa-moon">&nbsp;</em></a>
-                                       </td>
-                                    </tr>
-                                 </tbody>
-                              </table>
-                              <div class="card-footer text-center"><button class="btn btn-secondary" type="button"><a href="festival-cincodemayo.html">More details</a></button></div>
-                           </div>
-         
-                        </div>
-                        <div class="col-xl-4 col-lg-6">
-                              <div class="card b">
-                                 <div class="card-header">
-                                    <div class="float-right">
-                                       <div class="badge badge-info">Popular</div>
-                                       <div class="badge badge-danger">New</div>
-                                    </div>
-                                    <h4 class="m-0">Cinco De Mayo</h4><small class="text-muted">A celebration of mexican culture</small>
-                                 </div>
-                                 <div class="card-body">
-                                    <div class="d-flex align-items-center">
-                                       <img class="card-image" src="img/festivals/cincodemayo.jpg" alt="cinco de mayo" />
-                                    </div>
-                                 </div>
-                                 <table class="table">
-                                    <tbody>
-                                       <tr>
-                                          <td><strong>Next on</strong></td>
-                                          <td>May 5, 2020</td>
-                                       </tr>
-                                       <tr>
-                                          <td><strong> Regions celebrated in</strong></td>
-                                          <td><a class="inline" href="#"><img class="rounded-circle thumb24" src="img/countries/mx.png" alt="mexico" /></a><a class="inline" href="#"><img class="rounded-circle thumb24" src="img/countries/us.jpg" alt="project member"/></a><a class="inline" href="#"><strong>+5</strong></a></td>
-                                       </tr>
-                                       <tr>
-                                          <td><strong> Celebrated by</strong></td>
-                                          <td><a href="" data-toggle="tooltip" data-title="Team leader" data-original-title="" title=""><img class="rounded-circle thumb24" src="img/user/03.jpg" alt="project member"/></a><a class="inline" href="#"><img class="rounded-circle thumb24" src="img/user/02.jpg" alt="project member"/></a><a class="inline" href="#"><img class="rounded-circle thumb24" src="img/user/01.jpg" alt="project member"/></a><a class="inline" href="#"><strong>&nbsp;+ 1,389</strong></a></td>
-                                       </tr>
-                                       <tr>
-                                          <td><strong>Related content</strong></td>
-                                          <td>
-                                             <a href="" data-toggle="tooltip" data-title="Dances" data-original-title="" title=""><em class="fas fa-hands">&nbsp;</em></a>
-                                             <a href="" data-toggle="tooltip" data-title="Dresses" data-original-title="" title=""><em class="fas fa-female">&nbsp;</em></a>
-                                             <a href="" data-toggle="tooltip" data-title="Music" data-original-title="" title=""><em class="fas fa-music">&nbsp;</em></a>
-                                             <a href="" data-toggle="tooltip" data-title="Recipes" data-original-title="" title=""><em class="fas fa-utensils">&nbsp;</em></a>
-                                          </td>
-                                       </tr>
-                                    </tbody>
-                                 </table>
-                                 <div class="card-footer text-center"><button class="btn btn-secondary" type="button"><a href="festival-cincodemayo.html">More details</a></button></div>
-                              </div>
-                           </div>
-                           <div class="col-xl-4 col-lg-6">
-                                 <div class="card b">
-                                    
-                                    <div class="card-body">
-                                       <div class="d-flex align-items-center card-footer" style="height: 160px; justify-content: center;">
-                                             <button class="btn btn-secondary" type="button"><a href="festivals.html">+16 More</a></button>
-                                       </div>
-                                    </div>
-                                    
-                                 </div>
-                              </div>         
-            
-                     </div>
+                 <div class="row">
+      
+                 <div class="col-xl-4 col-lg-6">
+                       <div class="card b">
+                          <div class="card-header">
+                             <div class="float-right">
+                                <div class="badge badge-info">Popular</div>
+                                <div class="badge badge-danger">New</div>
+                             </div>
+                             <h4 class="m-0">Santana - Oye Como Va</h4><small class="text-muted">&nbsp;</small>
+                          </div>
+                          <div class="card-body">
+                             <div class="d-flex align-items-center">
+                                <img class="card-image" src="img/music/cincodemayo.png" alt="cinco de mayo" />
+                             </div>
+                          </div>
+                          <table class="table">
+                             <tbody>
+                                <tr>
+                                   <td><strong>Source Link</strong></td>
+                                   <td><a href="">Santana - Oye Como Va</a></td>
+                                </tr>
+                                <tr>
+                                   <td><strong>Related content</strong></td>
+                                   <td>
+                                      <a href="" data-toggle="tooltip" data-title="Dances" data-original-title="" title=""><em class="fas fa-hands">&nbsp;</em></a>
+                                      <a href="" data-toggle="tooltip" data-title="Dresses" data-original-title="" title=""><em class="fas fa-female">&nbsp;</em></a>
+                                      <a href="" data-toggle="tooltip" data-title="Festivals" data-original-title="" title=""><em class="fas fa-moon">&nbsp;</em></a>
+                                   </td>
+                                </tr>
+                             </tbody>
+                          </table>
+                          <div class="card-footer text-center"><button class="btn btn-secondary" type="button"><a href="festival-cincodemayo.html">More details</a></button></div>
+                       </div>
+     
+                    </div>
+                    <div class="col-xl-4 col-lg-6">
+                          <div class="card b">
+                             <div class="card-header">
+                                <div class="float-right">
+                                   <div class="badge badge-info">Popular</div>
+                                   <div class="badge badge-danger">New</div>
+                                </div>
+                                <h4 class="m-0">Cinco De Mayo</h4><small class="text-muted">A celebration of mexican culture</small>
+                             </div>
+                             <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                   <img class="card-image" src="img/festivals/cincodemayo.jpg" alt="cinco de mayo" />
+                                </div>
+                             </div>
+                             <table class="table">
+                                <tbody>
+                                   <tr>
+                                      <td><strong>Next on</strong></td>
+                                      <td>May 5, 2020</td>
+                                   </tr>
+                                   <tr>
+                                      <td><strong> Regions celebrated in</strong></td>
+                                      <td><a class="inline" href="#"><img class="rounded-circle thumb24" src="img/countries/mx.png" alt="mexico" /></a><a class="inline" href="#"><img class="rounded-circle thumb24" src="img/countries/us.jpg" alt="project member"/></a><a class="inline" href="#"><strong>+5</strong></a></td>
+                                   </tr>
+                                   <tr>
+                                      <td><strong> Celebrated by</strong></td>
+                                      <td><a href="" data-toggle="tooltip" data-title="Team leader" data-original-title="" title=""><img class="rounded-circle thumb24" src="img/user/03.jpg" alt="project member"/></a><a class="inline" href="#"><img class="rounded-circle thumb24" src="img/user/02.jpg" alt="project member"/></a><a class="inline" href="#"><img class="rounded-circle thumb24" src="img/user/01.jpg" alt="project member"/></a><a class="inline" href="#"><strong>&nbsp;+ 1,389</strong></a></td>
+                                   </tr>
+                                   <tr>
+                                      <td><strong>Related content</strong></td>
+                                      <td>
+                                         <a href="" data-toggle="tooltip" data-title="Dances" data-original-title="" title=""><em class="fas fa-hands">&nbsp;</em></a>
+                                         <a href="" data-toggle="tooltip" data-title="Dresses" data-original-title="" title=""><em class="fas fa-female">&nbsp;</em></a>
+                                         <a href="" data-toggle="tooltip" data-title="Music" data-original-title="" title=""><em class="fas fa-music">&nbsp;</em></a>
+                                         <a href="" data-toggle="tooltip" data-title="Recipes" data-original-title="" title=""><em class="fas fa-utensils">&nbsp;</em></a>
+                                      </td>
+                                   </tr>
+                                </tbody>
+                             </table>
+                             <div class="card-footer text-center"><button class="btn btn-secondary" type="button"><a href="festival-cincodemayo.html">More details</a></button></div>
+                          </div>
+                       </div>
+                       <div class="col-xl-4 col-lg-6">
+                             <div class="card b">
+                                
+                                <div class="card-body">
+                                   <div class="d-flex align-items-center card-footer" style="height: 160px; justify-content: center;">
+                                         <button class="btn btn-secondary" type="button"><a href="festivals.html">+16 More</a></button>
+                                   </div>
+                                </div>
+                                
+                             </div>
+                          </div>         
+        
+                 </div>
+{/*
                      <h3>Popular</h3>
                      <div class="row">
                            <div class="col-xl-4 col-lg-6">
@@ -246,9 +245,9 @@ class IndexPage extends React.Component {
                               </div>  
                         </div>
                </div>
- 
-            </div>
  */}
+            </div>
+    </div>
           <div>
             <p>
               Welcome to your new Gatsby + Netlify Functions + Netlify Identity
